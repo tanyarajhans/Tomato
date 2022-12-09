4e7b2349321827f909078aa3ded106fa9aa58841
adityaajay29 <adityaajay20142662@gmail.com>
2022-12-09 11:18:51 +0530

    updated tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-=======
   content: ["./src/**/*.{html,js}"],
->>>>>>> 428dcd55327a283db4a6b5ab911038d0915f0038
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 428dcd55327a283db4a6b5ab911038d0915f0038
