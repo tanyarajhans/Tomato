80b003727055794cef382c3a3a1336b9f9256ab8
adityaajay29 <adityaajay20142662@gmail.com>
2022-12-22 13:34:43 +0530

    checkout

---
+++ src/hooks/useTabSwitch.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+const useTabSwitch = (tab, defaultTab) => {
+    const [currentTab, setCurrentTab] = useState(defaultTab);
+
+    useEffect(() => {
+        setCurrentTab(defaultTab)
+    }, [defaultTab])
+
+    const handleTabSwitch = (tab) => {
+        setCurrentTab(tab);
+    } 
+    return [currentTab, handleTabSwitch];
+}
+
+export default useTabSwitch;
