f2bb6f5ad40ff112ba28578cf3d921e222f6f6f8
Tanya <tanyarajhans7@gmail.com>
2022-12-23 23:49:40 +0530

    Added items in cart summary to be displayed while checkout

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -3,6 +3,7 @@ import useTabSwitch from "../../hooks/useTabSwitch";
 import { Tabs } from "../../components/Tabs";
 import { cartProducts } from "../../stores/cart/cartSlice";
 import { AddressForm } from "../../components/AddressForm";
+import { ProductsSummary } from "../../components/ProductsSummary";
 
 const Cart = () => {
     const cart = useSelector(cartProducts);
@@ -20,7 +21,7 @@ const Cart = () => {
         <div className="bg-white h-screen text-black mx-auto mt-2 border border-gray-200 p-4 md:w-2/3 rounded-lg shadow-md sm:p-6 lg:p-8">
             <Tabs list={tabs} onTabSwitch={handleTabSwitch} activeTab={currentTab}></Tabs>
             <div className={`tabs ${currentTab !=='Summary' ? 'hidden' : ''}`}>
-                Summary
+                <ProductsSummary cartProducts={cart}></ProductsSummary>
             </div>
             <div className={`tabs ${currentTab !=='Delivery' ? 'hidden' : ''}`}>
                 <AddressForm onTabSwitch={handleTabSwitch}></AddressForm>
