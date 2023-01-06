9b045f4c2c39350d7cf0f5175a6bca55afab7542
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-01-06 23:27:51 +0530

    fixed homepage style and added more tailwindcss classes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,17 +5,17 @@ import Button from "./elements/Button";
 
 export const Banner = ()=>{
     return(
-        <div className="banner w-full md:w-2/3 px-7 mx-auto relative flex items-center-justify-between">
+        <div className="banner w-full mt-4 md:w-2/3 px-7 mx-auto relative flex items-center justify-between">
             <div className="banner-description w-full md:w-1/2 p-3">
                 <h2 className="mb-6 text-4xl text-white font-bold">
                     Order your Favorite dishes
                 </h2>
-                <p className="font-semibold text-lg text-red-600 py-2">
+                <p className="font-semibold text-sm text-red-600 py-2">
                     Get Started Today!
                 </p>
                 <div className="btn-container">
-                    <Button>Order Now</Button>
-                    <Link to = "/Menu" className="text-yellow-400 hover:text-yellow-500 font-bold text-decoration-line px-3">
+                    <Button className="bg-gradient-to-r from-yellow-400 to-yellow-500">Order Now</Button>
+                    <Link to = "/Menu" className="text-yellow-400 uppercase text-sm my-2 hover:text-yellow-500 font-bold text-decoration-line px-3">
                         see menu
                     </Link>
                 </div>
